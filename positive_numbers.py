a6807951c8648aba1c03a5401fa7722b21daaed5
kriti1807 <83088348+kriti1807@users.noreply.github.com>
2021-09-30 10:18:13 -0400

    Add files via upload

---
+++ positive_numbers.py
@@ -0,0 +1,11 @@
+L1=[12,-7,5,64,-14]
+L2=[12,14,-95,3]
+print("Positive numbers for list 1:\n")
+for n in L1:
+    if n>=0:
+        print(n)
+
+print("\nPositive numbers for list 2:\n")
+for m in L2:
+    if m>=0:
+        print(m)
